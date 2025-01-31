4e0da38175e215cb0abd651c66d21b820d19ee1b
Juhana Honkanen <juhgu@hotmail.com>
2025-01-31 02:50:30 +0200

    Clarified scene template and added palette reference

--- CMDG/SceneTemplate.cs
+++ CMDG/SceneTemplate.cs
@@ -14,8 +14,15 @@
             {
                 SceneControl.StartFrame();         // Clears frame buffer and starts frame timer.
 
-                // Stuff inside frame loop
-                // e.g. Framebuffer.SetPixel(x, y, color);
+                /* Stuff inside frame loop
+                e.g. Framebuffer.SetPixel(x, y, color);
+                
+                Values for SetPixel:
+                x: 0 to Config.ScreenWidth, default 0-200 
+                y: 0 to Config.ScreenHeight, default 0-100
+                color = RGB bytes in a Color32 struct, e.g. New Color32(255,255,255). Alpha channel is not used and does not exist.
+                color will be converted to nearest ANSI color, see palette.png for reference
+                */
 
                 SceneControl.EndFrame();          // Calculates spent time, limits to max framerate, and allows quitting by pressing ESC.
             }
