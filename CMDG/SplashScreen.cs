d0d92b47090fb53881d54f11cfb8c3f6aaccdf64
Juhana Honkanen <juhgu@hotmail.com>
2025-02-19 17:42:03 +0200

    Splash screen v2

--- CMDG/SplashScreen.cs
+++ CMDG/SplashScreen.cs
@@ -15,28 +15,44 @@ namespace CMDG
                     using (Bitmap bitmap = new Bitmap(imagePath))
                     {
                         Console.Clear();
+                        int splashFrameRate = 50;
 
-                        for (int y = 0; y < bitmap.Height; y++)
-                        {
-                            for (int x = 0; x < bitmap.Width; x++)
-                            {
-                                Color pixel = bitmap.GetPixel(x, y);
-
-                                if (!(pixel.R != 255 && pixel.G != 255 && pixel.B != 255))
-                                {
-                                    Console.SetCursorPosition(x, y);
-                                    Console.Write("█");
-                                }
-                            }
-                        }
+                        DrawBitmap(bitmap, '·', splashFrameRate);
+                        DrawBitmap(bitmap, '•', splashFrameRate);
+                        DrawBitmap(bitmap, '#', splashFrameRate);
+                        DrawBitmap(bitmap, '▓', splashFrameRate);
+                        DrawBitmap(bitmap, '█', splashFrameRate);
+                        Thread.Sleep(2000);
+                        DrawBitmap(bitmap, '▓', splashFrameRate);
+                        DrawBitmap(bitmap, '#', splashFrameRate);
+                        DrawBitmap(bitmap, '•', splashFrameRate);
+                        DrawBitmap(bitmap, '·', splashFrameRate);
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine("Error: " + ex.Message);
                 }
-                Thread.Sleep(5000);
             }
+        }
+
+
+        public static void DrawBitmap(Bitmap bitmap, char character, int splashFrameRate)
+        {
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+
+                    if (!(pixel.R != 255 && pixel.G != 255 && pixel.B != 255))
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(character);
+                    }
+                }
+            }
+            Thread.Sleep(splashFrameRate);
         }
     }
 }
